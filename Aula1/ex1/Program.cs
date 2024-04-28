66f54e3341d68e16d0b47a9530bea6f731c31dd3
01-Zick <zickmanhozo@gmail.com>
2024-04-28 12:22:33 -0300

    Anotações aula 1

---
+++ Aula1/ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      String frase = scan.nextLine();
+      String[] palavras = frase.split(" ");
+      
+      for (int i = 0; i < palavras.length; i++){
+        if (i != palavras.length - 1) {
+          System.out.println(palavras[i]);
+        }else {
+          imprimirPalavra(palavras[i]);
+        }
+      }
+      
+  }
+  
+  
+  public static void imprimirPalavra(String palavra){
+    char[] characteres = palavra.toCharArray();
+    
+     for (char character : characteres) {
+        System.out.println(character);
+      }
+  }
+  
+  
+  
+}
---
+++ Aula1/ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿import java.util.*;
+
+public class Main {
+  
+  public static final String USUARIO = "MeuNomeNaoEhZelda";
+  public static final String SENHA = "meuNomeEHL1nk";
+  
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      boolean usuarioEstahAutenticado;
+      
+      do{
+        String usuario = scan.nextLine();
+        String senha = scan.nextLine();
+        
+        usuarioEstahAutenticado = validarUsuario(usuario, senha);
+        
+        if (!usuarioEstahAutenticado){
+          
+          System.out.println("Usuário e/ou senha inválidos!");
+        }else {
+          
+          System.out.println("Seja bem vindo! " + USUARIO);
+        }
+        
+      }while (!usuarioEstahAutenticado);
+  }
+  
+  public static boolean validarUsuario(String usuario, String senha){
+    
+    if (!usuario.equalsIgnoreCase(USUARIO)){
+      return false;
+    }
+    
+    if (!senha.equals(SENHA)){
+      return false;
+    }
+    return true;
+  }
+}
